4218e40f6930136379a73d9794b1a477e2ad4b6e
Artur Komoter <artur@komoter.pl>
2020-04-05 18:30:52 +0100

    Create WebStorm file templates for components and stories

---
+++ .idea/fileTemplates/CADteams Component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+//import classNames from 'classnames';
+
+//import styles from './${Component}.module.scss';
+
+function $Component({}) {
+  return ();
+}
+
+${Component}.propTypes = {};
+
+${Component}.defaultProps = {};
+
+export default $Component;
