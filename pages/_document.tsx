d167767c9c70a054e068e65c3d3f78c4fd04de83
Artur Komoter <artur@komoter.pl>
2020-04-14 22:18:49 +0200

    Custom Next.js document component

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en-GB">
+        <Head>
+          <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="description" content="A platform for CAD and BIM specialists to showcase their best building designs." />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
