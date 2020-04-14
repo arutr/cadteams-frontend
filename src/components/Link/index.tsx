83c664873630f895f2c22794263f5b5529ba8335
Artur Komoter <artur@komoter.pl>
2020-04-14 21:18:14 +0200

    Link component accepts click event handlers #2

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -11,21 +11,28 @@ import styles from './Link.module.scss';
  * @param children Link text
  * @param icon Left icon for the link - this also controls whether the link is underlined!
  * @param external Is this an external link? If so, create a standard anchor tag
+ * @param onClick Click event handler
  * @constructor
  */
 function Link({
-  href, children, icon, external,
+  href, children, icon, external, onClick,
 }) {
   return external
     ? <a href={href} target="_blank" rel="noopener noreferrer">{children}</a>
     : (
       <NextLink href={href}>
         {icon ? (
-          <span className={classNames(styles.link, icon && `icon icon__${icon}`)}>
+          <span
+            className={classNames(styles.link, icon && `icon icon__${icon}`)}
+            onClick={onClick}
+            role="link"
+            tabIndex={0}
+            onKeyPress={onClick}
+          >
             {children}
           </span>
         ) : (
-          <a>{children}</a>
+          <a onClick={onClick} role="link" tabIndex={0} onKeyPress={onClick}>{children}</a>
         )}
       </NextLink>
     );
@@ -36,6 +43,7 @@ Link.propTypes = {
   icon: PropTypes.string,
   children: PropTypes.node,
   external: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 Link.defaultProps = {
@@ -43,6 +51,7 @@ Link.defaultProps = {
   icon: '',
   children: null,
   external: false,
+  onClick: null,
 };
 
 export default Link;
