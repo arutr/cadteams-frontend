e7eb213b23bef5c78d22021e60ef1234ed1c1367
Artur Komoter <artur@komoter.pl>
2020-04-14 21:01:18 +0200

    Remove decorator from Storybook file template

--- .idea/fileTemplates/Storybook Story༏ext༏.stories.tsx
+++ .idea/fileTemplates/Storybook Story༏ext༏.stories.tsx
@@ -2,10 +2,5 @@ import React from 'react';
 import ${NAME} from '../src/components/${NAME}';
 
 export default {
-  decorators: [(story) => (
-    <section style={{ padding: '1rem' }}>
-      {story()}
-    </section>
-  )],
   title: '${NAME}',
 };
