674128dbff92ebbbfd6caa7ff90b95581f722e24
Artur Komoter <artur@komoter.pl>
2020-04-20 18:38:26 +0200

    Update footer #6
    + Add link to T&C
    + Remove Legal link
    + Add social media links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,15 +3,52 @@ import Link from '../Link';
 
 import styles from './Footer.module.scss';
 
+const socialMedia = [
+  {
+    name: 'facebook',
+    href: 'https://facebook.com/cadteams',
+  },
+  {
+    name: 'instagram',
+    href: 'https://instagram.com/cadteams',
+  },
+  {
+    name: 'linkedin',
+    href: 'https://linkedin.com/company/cadteams',
+  },
+  {
+    name: 'medium',
+    href: 'https://medium.com/cadteams-magazine',
+  },
+];
+
 function Footer() {
   return (
     <footer className={styles.footer}>
       <ul>
-        <span>
-          &copy; {new Date().getFullYear()} <b>CAD</b>teams
-        </span>
-        <Link external>Terms and Conditions</Link>
-        <Link external>Legal</Link>
+        <div>
+          <span>
+            &copy; {new Date().getFullYear()} <b>CAD</b>teams
+          </span>
+          <Link
+            external
+            href="https://drive.google.com/uc?id=1MuNKTTASbRC7NhVGxbMNxtwDDdhxrOe9"
+          >
+            Terms and Conditions
+          </Link>
+          <a href="mailto:hello@cadteams.com">Contact Us</a>
+        </div>
+        <div className={styles.social}>
+          {socialMedia.map(({ name, href }, index) => (
+            <Link
+              key={index}
+              className="icon__large"
+              icon={name}
+              href={href}
+              external
+            />
+          ))}
+        </div>
       </ul>
     </footer>
   );
