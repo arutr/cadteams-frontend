257abe1156a11908bec69bec40a99022773fa539
Artur Komoter <artur@komoter.pl>
2020-04-24 23:24:47 +0200

    Fix viewport height for mobile #12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Head from 'next/head';
+import debounce from 'lodash/debounce';
 import { Amplitude, AmplitudeProvider } from 'react-amplitude-hooks';
 import { AmplitudeClient } from 'amplitude-js';
 import { isBrowser } from '@unly/utils';
@@ -11,6 +12,18 @@ export default function MyApp({ Component, pageProps }) {
   if (!isBrowser()) {
     return <Component {...pageProps} />;
   }
+
+  function handleResize() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  useEffect(() => {
+    handleResize();
+    const debouncedResize = debounce(handleResize, 500);
+    window.addEventListener('resize', debouncedResize);
+    return () => window.removeEventListener('resize', debouncedResize);
+  });
 
   // eslint-disable-next-line global-require
   const amplitude = require('amplitude-js');
