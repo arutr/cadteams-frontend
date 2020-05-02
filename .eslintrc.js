682ae7d0800ceab49841313095ed3d3859da8447
Artur Komoter <artur@komoter.pl>
2020-05-02 22:19:30 +0200

    Disable more linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,12 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': 0,
     'jsx-a11y/anchor-is-valid': 0,
+    'no-console': 0,
+    'react/button-has-type': 0,
     'react/no-array-index-key': 0,
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': 0,
+    'react/jsx-no-target-blank': 0,
+    'react/forbid-prop-types': 0,
   }
 };
