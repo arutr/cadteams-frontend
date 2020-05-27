c4ac7632d3b143bec7e8b61bb4457717a2bb80de
Artur Komoter <artur@komoter.pl>
2020-05-27 19:15:05 +0200

    Contact information is editable in the same way as other profile sections #26

--- src/components/Portfolio/PortfolioFooter.tsx
+++ src/components/Portfolio/PortfolioFooter.tsx
@@ -1,24 +1,22 @@
 import classNames from 'classnames';
-import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 import React from 'react';
 import { useFormContext } from 'react-hook-form';
-import Button from 'src/components/Button';
-import { Error, Input } from 'src/components/Form';
+import { Error } from 'src/components/Form';
 import { Heading2 } from 'src/components/Heading';
 import Icon from 'src/components/Icon';
-import { EditableTextArea, Placeholder } from 'src/components/Portfolio/Editable';
+import { EditableInput, EditableTextArea, Placeholder } from 'src/components/Portfolio/Editable';
 import EditButton from 'src/components/Portfolio/EditButton';
 import { PortfolioSectionProps } from 'src/components/Portfolio/index';
 import styles from 'src/components/Portfolio/Portfolio.module.scss';
-import { useAuth } from 'src/contexts/AuthProvider';
 import ProfileUpdateProvider from 'src/contexts/ProfileUpdateContext';
+import validator from 'validator';
 
-interface UpdateFormValues {
+interface DescriptionUpdateFormValues {
   description: string;
 }
 
 function DescriptionUpdateForm({ isProfile, user }: PortfolioSectionProps) {
-  const { register } = useFormContext<UpdateFormValues>();
+  const { register } = useFormContext<DescriptionUpdateFormValues>();
   const placeholder = `A short description of your ${user?.type === 'individual'
     ? 'professional experience'
     : 'company'}.`;
@@ -52,50 +50,37 @@ interface ContactInformationFormValues {
   phone: string;
 }
 
-function ContactInformationForm({ user, setDialog }: PortfolioSectionProps) {
-  const { updateUser } = useAuth();
-  const { register, errors, handleSubmit } = useFormContext<ContactInformationFormValues>();
-  const onSubmit = handleSubmit((values) => {
-    setDialog(null);
-    updateUser(values).catch((error) => {
-      setDialog({
-        type: 'error',
-        message: `An unexpected error has occurred whilst updating contact information. Please try
-        again later.`,
-      });
-
-      if (PHASE_DEVELOPMENT_SERVER) {
-        throw error;
-      }
-    });
-  });
+function ContactInformationForm({ isProfile, user }: PortfolioSectionProps) {
+  const { register, errors } = useFormContext<ContactInformationFormValues>();
 
   return (
     <>
       <p>
         These details will be shared with other <b>CAD</b>teams members.
       </p>
-      <form onSubmit={onSubmit}>
-        <Input
-          className={styles.fieldset}
-          label={<Icon large name="phone" title="Phone number" />}
+      <form>
+        <Icon className={styles.icon} large name="phone" title="Phone number" />
+        <EditableInput
           defaultValue={user?.phone}
           placeholder="+44 1234 567890"
-          id="phone"
+          name="phone"
           type="tel"
           ref={register({
-            minLength: {
-              value: 11,
-              message: 'Please enter a valid UK phone number.',
-            },
-            maxLength: {
-              value: 13,
-              message: 'Please enter a valid UK phone number.',
-            },
+            validate: (value) => (
+              !value.length
+              || validator.isMobilePhone(value, 'en-GB')
+              || 'Please enter a valid UK phone number.'
+            ),
           })}
-        />
+        >
+          <Placeholder
+            isProfile={isProfile}
+            publicValue=""
+            profileValue="Contact phone number"
+            value={<a href={`tel:${user?.phone}`}>{user?.phone}</a>}
+          />
+        </EditableInput>
         <Error className={styles.error} filler={false} errors={errors} name="phone" />
-        <Button block>Update</Button>
       </form>
     </>
   );
@@ -105,14 +90,21 @@ export default function PortfolioFooter(props: PortfolioSectionProps) {
   const { setDialog, isProfile } = props;
 
   return (
-    <ProfileUpdateProvider<UpdateFormValues> setDialog={setDialog}>
-      <footer className={styles.row}>
+    <footer className={styles.row}>
+      <ProfileUpdateProvider<DescriptionUpdateFormValues> setDialog={setDialog}>
         <DescriptionUpdateForm {...props} />
+      </ProfileUpdateProvider>
+      <ProfileUpdateProvider<ContactInformationFormValues> setDialog={setDialog}>
         <section className={classNames(styles.card, styles['contact-information'])}>
           <Heading2 bold condensed marginTop={0}>Contact Information</Heading2>
-          {isProfile && <ContactInformationForm {...props} />}
+          {isProfile && (
+            <>
+              <EditButton />
+              <ContactInformationForm {...props} />
+            </>
+          )}
         </section>
-      </footer>
-    </ProfileUpdateProvider>
+      </ProfileUpdateProvider>
+    </footer>
   );
 }
