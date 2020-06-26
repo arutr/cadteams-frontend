668f725a6bf6ed086e366392e554e8f54c564af2
Artur Komoter <artur@komoter.pl>
2020-06-26 14:40:02 +0200

    Add subpage links to guest navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import classNames from 'classnames';
 import dynamic from 'next/dynamic';
-import Router, { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { AnchorButton } from 'src/components/Button';
+import Label from 'src/components/Label';
 import { useAuth } from 'src/contexts/AuthProvider';
+import { inApp } from 'src/utils/misc';
 import getApiResource from '../../utils/api';
 import Icon from '../Icon';
+import Link from '../Link';
 import MediaObject from '../MediaObject';
 import styles from './Navigation.module.scss';
-import Link from '../Link';
 import AppNavigationLinks from './VerticalNavigationLinks';
 
 export function GuestNavigationLinks() {
@@ -16,13 +19,23 @@ export function GuestNavigationLinks() {
   if (!isAuthenticated()) {
     return (
       <>
-        <Link href="/sign-up"><Icon name="edit" /> Sign Up</Link>
-        <Link href="/log-in"><Icon name="sign-in" /> Log In</Link>
+        <div className={styles.users}>
+          <Link href="/individuals">Specialists</Link>
+          <Link href="/enterprise">Enterprise</Link>
+          <Link href="/outsourcing">Outsourcing</Link>
+        </div>
+        <span className={styles.separator} />
+        <Link className={styles['log-in']} href="/log-in">
+          <Icon name="sign-in" /> Log In
+        </Link>
+        <AnchorButton className={styles['sign-up']} href="/sign-up">
+          Sign Up
+        </AnchorButton>
       </>
     );
   }
 
-  if (Router.pathname.startsWith('/app')) {
+  if (inApp()) {
     return (
       <>
         <Link onClick={logOut}><Icon name="sign-out" /> Log Out</Link>
@@ -37,7 +50,7 @@ export function GuestNavigationLinks() {
       <Link href="/app/profile" hoverEffect={false}>
         <MediaObject
           captionAlign="left"
-          className={styles.avatar}
+          className={classNames(styles.avatar, !profilePicture && styles.blank)}
           src={getApiResource(profilePicture?.formats?.thumbnail?.url, profilePicture?.url)
           ?? '/icons/user-blank.svg'}
           vertical
@@ -72,11 +85,12 @@ function Navigation({ guest }: Props) {
     <header className={guest ? styles.horizontal : styles.vertical}>
       <nav>
         <Link
-          href={isAuthenticated() ? '/app/profile' : '/'}
+          href={guest ? '/' : '/app/profile'}
           hoverEffect={false}
           className={styles.logo}
         >
           <img src="/images/logo.svg" alt="CADteams" />
+          <Label className={styles.beta} small>Beta</Label>
         </Link>
         <Icon className={styles.toggle} name={open ? 'close' : 'menu'} onClick={toggleMenu} />
         <div className={classNames(styles.links, open && styles.open)}>
