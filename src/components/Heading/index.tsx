78501b36a1a02a9a4fe247d32f4bb4c9016f8013
Artur Komoter <artur@komoter.pl>
2020-06-26 14:38:28 +0200

    Refactor Heading component

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
-
 import styles from './Heading.module.scss';
 
+interface Props {
+  bold?: boolean;
+  children: any;
+  className?: string;
+  condensed?: boolean;
+  level?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  marginTop?: string;
+  marginBottom?: string;
+}
+
 function BaseHeading({
-  bold, children, condensed, level, marginTop, marginBottom,
-}) {
+  bold, children, className, condensed, level, marginTop, marginBottom,
+}: Props) {
   const Level = level;
   return (
     <Level
-      className={classNames(bold && styles.bold, condensed && styles.condensed)}
+      className={classNames(bold && styles.bold, condensed && styles.condensed, className)}
       style={{ marginTop, marginBottom }}
     >
       {children}
@@ -18,27 +26,9 @@ function BaseHeading({
   );
 }
 
-export const Heading1 = (props) => BaseHeading({ ...props, level: 'h1' });
-export const Heading2 = (props) => BaseHeading({ ...props, level: 'h2' });
-export const Heading3 = (props) => BaseHeading({ ...props, level: 'h3' });
-export const Heading4 = (props) => BaseHeading({ ...props, level: 'h4' });
-export const Heading5 = (props) => BaseHeading({ ...props, level: 'h5' });
-export const Heading6 = (props) => BaseHeading({ ...props, level: 'h6' });
-
-BaseHeading.propTypes = {
-  bold: PropTypes.bool,
-  children: PropTypes.node,
-  condensed: PropTypes.bool,
-  level: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
-  marginTop: PropTypes.string,
-  marginBottom: PropTypes.string,
-};
-
-BaseHeading.defaultProps = {
-  bold: false,
-  children: null,
-  condensed: false,
-  level: 'h1',
-  marginTop: null,
-  marginBottom: null,
-};
+export const Heading1 = (props: Props) => BaseHeading({ ...props, level: 'h1' });
+export const Heading2 = (props: Props) => BaseHeading({ ...props, level: 'h2' });
+export const Heading3 = (props: Props) => BaseHeading({ ...props, level: 'h3' });
+export const Heading4 = (props: Props) => BaseHeading({ ...props, level: 'h4' });
+export const Heading5 = (props: Props) => BaseHeading({ ...props, level: 'h5' });
+export const Heading6 = (props: Props) => BaseHeading({ ...props, level: 'h6' });
